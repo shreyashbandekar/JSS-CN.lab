1386564794eeabdc62d78e731fcc9ee484418f95
My Name <shreyash.bandekar2015@gmail.com>
2024-01-07 17:38:02 +0530

    SimpleCRC.java

--- SimpleCRC.java
+++ SimpleCRC.java
@@ -19,27 +19,20 @@ public class SimpleCRC {
         }
         return Integer.toHexString(crc).toUpperCase();
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the data for CRC calculation: ");
         String inputData = scanner.nextLine().trim();
         String calculatedCRC = calculateCRC(inputData);
-
         System.out.println("Calculated CRC: " + calculatedCRC);
-
         System.out.print("Enter the received data (message + CRC): ");
         String receivedData = scanner.nextLine().trim();
         String receivedMessage = receivedData.substring(0, receivedData.length() - calculatedCRC.length());
         String receivedCRC = receivedData.substring(receivedData.length() - calculatedCRC.length());
-
         if (calculatedCRC.equals(receivedCRC))
             System.out.println("CRC Check: Data is intact. Received message: " + receivedMessage);
         else
             System.out.println("CRC Check: Data is corrupted. Discarding the message.");
-
-        
-        scanner.close();
+            scanner.close();
     }
 }
